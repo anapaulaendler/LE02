8e9cd30a4999d28b0a2f8983c477d720882df74d
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 09:55:32 -0300

    Exercicio Quatorze

---
+++ ExercicioQuatorze.java
@@ -0,0 +1,32 @@
+public class ExercicioQuatorze {
+    public static void executar() {
+    /* Faça um programa que leia um vetor de 5 elementos e, após a leitura, posicione 
+    o maior elemento na última posição do vetor. Nenhum número do vetor pode ser 
+    apagado ou duplicado. Apresente o vetor atualizado na tela.  */
+
+    int[] n = new int[5];
+    int maior = 0;
+    int j = 0;
+
+    for (int i = 0; i < 5; i++) {
+        n[i] = Prompt.lerInteiro("Digite o valor da posição " + (i + 1) + ":");
+        
+        if (n[i] > maior) {
+            maior = n[i];
+            j = i;
+        }
+    }
+
+    n[j] = n[4];
+    n[4] = maior;
+
+    System.out.print("A nova ordem de valores do vetor é: ");
+
+    for (int i = 0; i < 4; i++) {
+        System.out.print(n[i] + ", ");
+    }
+
+    System.out.println(n[4] + ".");
+    Prompt.separador();
+}
+}
--- Programa.java
+++ Programa.java
@@ -1,7 +1,7 @@
 public class Programa {
 
     public static void main(String[] args) {
-        ExercicioTreze.executar();
+        ExercicioQuatorze.executar();
     }
 
 }
