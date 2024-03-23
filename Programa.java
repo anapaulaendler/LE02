5e1ff7a6dfd32d32aa4742e4153df7d6f161195d
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-23 10:12:28 -0300

    Alterações Finais

--- Programa.java
+++ Programa.java
@@ -1,6 +1,20 @@
 public class Programa {
 
     public static void main(String[] args) {
+        ExercicioUm.executar();
+        ExercicioDois.executar();
+        ExercicioTres.executar();
+        ExercicioQuatro.executar();
+        ExercicioCinco.executar();
+        ExercicioSeis.executar();
+        ExercicioSete.executar();
+        ExercicioOito.executar();
+        ExercicioNove.executar();
+        ExercicioDez.executar();
+        ExercicioOnze.executar();
+        ExercicioDoze.executar();
+        ExercicioTreze.executar();
+        ExercicioQuatorze.executar();
         ExercicioQuinze.executar();
     }
 
