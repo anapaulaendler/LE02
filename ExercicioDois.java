cc95c1ae362b0d92c7c8573606c3c13d9b76ce3b
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-22 22:13:39 -0300

    Exercício Dois

---
+++ ExercicioDois.java
@@ -0,0 +1,31 @@
+public class ExercicioDois {
+    public static void executar() {
+        /* Escreva um algoritmo que leia em um vetor uma sequência finita de números
+        digitados pelo usuário e, logo após, mostre o número de cada posição do vetor e se
+        ele é positivo, negativo ou zero.  */
+
+
+        int n = Prompt.lerInteiro("Digite a quantidade de casos: ");
+
+
+        int[] num = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            num[i] = Prompt.lerInteiro("Digite seu número: ");
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            if (num[i] > 0) {
+                System.out.println(num[i] + " é positivo!");
+            } else if (num[i] < 0) {
+                System.out.println(num[i] + " é negativo!");
+            } else {
+                System.out.println(num[i] + " é igual a zero!");
+            }
+        }
+    }
+ }
+
+
--- Programa.java
+++ Programa.java
@@ -2,6 +2,7 @@ public class Programa {
 
     public static void main(String[] args) {
         ExercicioUm.executar();
+        ExercicioDois.executar();
     }
 
 }
