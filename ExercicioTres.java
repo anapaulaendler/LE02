1844116b3788ce2682e2268c2fce5802cd210b45
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-22 22:25:50 -0300

    Terceiro Exercício

---
+++ ExercicioTres.java
@@ -0,0 +1,25 @@
+public class ExercicioTres {
+    public static void executar() {
+        /* Faça um programa que leia em um vetor uma sequência finita de números digitados 
+        pelo usuário. Crie um segundo vetor que armazene o dobro de cada número do 
+        primeiro vetor e depois apresente os valores deste vetor. */
+
+        int n = Prompt.lerInteiro("Digite o tamanho do vetor: ");
+        int[] num = new int[n];
+        int[] num2 = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = Prompt.lerInteiro("Digite seu número: ");
+            num2[i] = 2 * num[i];
+        }
+
+        System.out.println("Seu novo vetor tem os seguintes valores: ");
+        
+        for (int i = 0; i < (n - 1); i++) {
+            System.out.print(num2[i] + ", ");
+        }
+
+        System.out.println(num2[n-1] + ".");
+        Prompt.separador();
+    }
+}
--- Programa.java
+++ Programa.java
@@ -1,8 +1,7 @@
 public class Programa {
 
     public static void main(String[] args) {
-        ExercicioUm.executar();
-        ExercicioDois.executar();
+        ExercicioTres.executar();
     }
 
 }
