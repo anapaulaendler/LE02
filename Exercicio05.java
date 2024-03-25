7695998a6ad5a36fbd25f42f6f3bed3f46bbef37
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 12:26:33 -0300

    Renomear 05

--- Exercicio05.java
+++ Exercicio05.java
@@ -1,4 +1,4 @@
-public class ExercicioCinco {
+public class Exercicio05 {
     public static void executar() {
         /*  Elabore um programa que leia 5 números do teclado e os armazene em um vetor.
         Em seguida, leia um número n qualquer e apresente na tela a quantidade de vezes
