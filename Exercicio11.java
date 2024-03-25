fc80b34129d9201973daa4f942cb3fd3cd0550f4
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 12:32:15 -0300

    Renomear 11

--- Exercicio11.java
+++ Exercicio11.java
@@ -1,4 +1,4 @@
-public class ExercicioOnze {
+public class Exercicio11 {
     public static void executar() {
         /* Elabore um algoritmo que calcule o produto escalar entre dois vetores de inteiros 
         de tamanho igual a 5. Exemplo: {0, 2, 4, 6, 8}, {1, 3, 5, 7, 9} = 0*1 + 2*3 + 4*5 + 6*7 + 
