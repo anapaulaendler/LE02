89c29a571d318d2337bd88d3b72c178bbfb3d128
Ana Paula Endler <anapaulaendler@gmail.com>
2024-03-25 12:29:28 -0300

    Renomear 08

--- Exercicio08.java
+++ Exercicio08.java
@@ -1,4 +1,4 @@
-public class ExercicioOito {
+public class Exercicio08 {
     public static void executar() {
         /*  Dados dois vetores A e B de 5 elementos cada, criar um vetor C que representa a 
         concatenação de A e B, ou seja, C contém os elementos de A seguidos dos elementos 
